cf423707b834768db94846cbc39b1dc1ee10fd05
Sergey Chernikov <sergey.chernikov.mail@gmail.com>
2026-03-05 18:12:53 +0500

    remove my key

--- task/utils/constants.py
+++ task/utils/constants.py
@@ -2,6 +2,6 @@ from os import getenv
 
 TOOL_CALL_HISTORY_KEY = "tool_call_history"
 CUSTOM_CONTENT = "custom_content"
-API_KEY = getenv('API_KEY', 'dial-bevmy8nn9t169udaejgvd94kuwy')
+API_KEY = getenv('API_KEY', '')
 API_VERSION = getenv('API_VERSION', '2025-01-01-preview')
 API_ENDPOINT = getenv('API_ENDPOINT', 'https://ai-proxy.lab.epam.com')
